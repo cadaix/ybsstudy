94ad7019abe4eb5db40c2b6aff3cb932a4ffe805
cadaix <bayarvet@gmail.com>
2022-10-14 16:26:21 +0300

    Url, views, templates yapıları oluşturuldu.

--- base/views.py
+++ base/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+rooms = [
+    {'id':1,
+    'name':'İşletme Giriş Bölümü'
+    },
+      {'id':2,
+    'name':'İşletme Matematiği Bölümü'
+    },
+      {'id':3,
+    'name':'Programlamaya Giriş Bölümü'
+    },
+      {'id':4,
+    'name':'İşletme Bilimine Giriş Bölümü'
+    },
+      {'id':5,
+    'name':'Bilgisayar Kullanımına Giriş Bölümü'
+    },
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    return render(request, 'base/room.html') 
