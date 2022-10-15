78e49787b02c133b71c8f8e19dca7a68b6d553aa
cadaix <bayarvet@gmail.com>
2022-10-15 13:48:10 +0300

    CRUD işlemlerine başlandı. Models database yapıları oluşturuldu.

--- base/views.py
+++ base/views.py
@@ -1,28 +1,42 @@
 from django.shortcuts import render
 
+from .models import Room
+from .forms import RoomForm
 
-rooms = [
-    {'id':1,
-    'name':'İşletme Giriş Bölümü'
-    },
-      {'id':2,
-    'name':'İşletme Matematiği Bölümü'
-    },
-      {'id':3,
-    'name':'Programlamaya Giriş Bölümü'
-    },
-      {'id':4,
-    'name':'İşletme Bilimine Giriş Bölümü'
-    },
-      {'id':5,
-    'name':'Bilgisayar Kullanımına Giriş Bölümü'
-    },
-]
+
+# rooms = [
+#     {'id':1,
+#     'name':'İşletme Giriş'
+#     },
+#       {'id':2,
+#     'name':'İşletme Matematiği'
+#     },
+#       {'id':3,
+#     'name':'Programlamaya Giriş'
+#     },
+#       {'id':4,
+#     'name':'İşletme Bilimine Giriş'
+#     },
+#       {'id':5,
+#     'name':'Bilgisayar Kullanımına Giriş'
+#     },
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    return render(request, 'base/room.html') 
+    room = Room.objects.get(id=pk)
+    
+    context = {'room' : room}
+
+    return render(request, 'base/room.html', context) 
+
+def createRoom(request):
+    form = RoomForm
+    context = {'form' : form }
+    return render(request,'base/room_form.html', context)
+     
