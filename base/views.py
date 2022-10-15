b477f7fa4c5ed6579009160fa76766f8f3139ebf
cadaix <bayarvet@gmail.com>
2022-10-15 16:34:53 +0300

    Search bar eklendi konular arasında filtreme özelliği eklendi

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -23,8 +24,12 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
@@ -37,6 +42,35 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
     context = {'form' : form }
     return render(request,'base/room_form.html', context)
+
+
+
+def updateRoom(request, pk):
+     room = Room.objects.get(id=pk)
+     form = RoomForm(instance=room)
+
+     if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+     context =  {'form' : form}
+     return render(request, 'base/room_form.html', context)
      
+
+def deleteRoom(request, pk ):
+    room = Room.objects.get(id=pk) 
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj' : room})
+    
