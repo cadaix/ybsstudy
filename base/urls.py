8ebb66ae4a1fc29b0fb37aa72e59968844b0f780
cadaix <bayarvet@gmail.com>
2022-10-17 15:43:27 +0300

    Kullanıcı Adı Giriş ekranı ve databe implantasyonu oluşturuldu 2:35:11

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 
 urlpatterns = [
+    
+    path('login/', views.loginPage, name='login'),
     path('', views.home , name='home'),
     path('room/<str:pk>/', views.room, name='room'),
 
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
-from multiprocessing import context
-from django.shortcuts import render, redirect
 
+from multiprocessing import context
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -23,13 +26,35 @@ from .forms import RoomForm
 #     },
 # ]
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Kullanıcı Adınız veya Şifreniz Hatalı')
+
+
+    context = {}
+    return render(request,'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q) 
+        )
 
     topics = Topic.objects.all()
 
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
