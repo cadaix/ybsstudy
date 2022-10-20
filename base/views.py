e369558114fc5bc587a39bfcb5d481a88e40a839
cadaix <bayarvet@gmail.com>
2022-10-20 16:21:48 +0300

    Css integration completed.

--- base/views.py
+++ base/views.py
@@ -130,15 +130,23 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+
+        )
+      
+        return redirect('home')
     
-    context = {'form' : form }
+    context = {'form' : form, 'topics': topics }
     return render(request,'base/room_form.html', context)
 
 
@@ -146,17 +154,21 @@ def createRoom(request):
 def updateRoom(request, pk):
      room = Room.objects.get(id=pk)
      form = RoomForm(instance=room)
+     topics = Topic.objects.all()
 
      if request.user != room.host:
         return HttpResponse('Bunu yapmak için yetkili değilsiniz.')
 
      if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-     context =  {'form' : form}
+     context =  {'form' : form, 'topics' : topics, 'room' : room}
      return render(request, 'base/room_form.html', context)
      
 @login_required(login_url='login')
@@ -183,3 +195,11 @@ def deleteMessage(request, pk ):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj' : message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    return render(request, 'base/update-user.html')
+
+
+     
