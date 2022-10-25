f722a1f8c181769e68dd3c363fa423ce6f22b39c
cadaix <bayarvet@gmail.com>
2022-10-25 09:44:00 +0300

    edit account

--- base/views.py
+++ base/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Message, Room, Topic
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 # rooms = [
@@ -199,7 +199,15 @@ def deleteMessage(request, pk ):
 
 @login_required(login_url='login')
 def updateUser(request):
-    return render(request, 'base/update-user.html')
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update-user.html', {'form':form})
 
 
      
