d686684b5c71c382ca7d04d7b657e0df17609810
cadaix <bayarvet@gmail.com>
2022-10-27 09:54:56 +0300

    api completed

--- base/views.py
+++ base/views.py
@@ -90,7 +90,7 @@ def home(request):
         Q(description__icontains=q) 
         )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
 
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -210,4 +210,13 @@ def updateUser(request):
     return render(request, 'base/update-user.html', {'form':form})
 
 
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics' : topics} )
+
+
      
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
--- ybsstudy/settings.py
+++ ybsstudy/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
+    "corsheaders",
     'base.apps.BaseConfig', 
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'ybsstudy.urls'
@@ -129,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
